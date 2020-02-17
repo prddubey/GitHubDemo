b692527746b9b495a8769d5ac4fa7daa6ec9f787
Prashant Dubey <pradubey@in.ibm.com>
2020-02-17 16:50:18 +0530

    commit file 17-02

---
+++ GitProjectDemo/src/main/java/com/demo/spring/GitProjectDemo/App.java
@@ -0,0 +1,13 @@
+package com.demo.spring.GitProjectDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
